717f263c41115bc36933e7ca3ee3d868c5ce01ca
Igor Srdoc <igor@srdoc.si>
2018-06-05 16:33:08 +0200

    update build tools version, add firebase ui login (email works, google auth throws error)

--- android/app/src/main/java/com/visionectmobileble/MainActivity.java
+++ android/app/src/main/java/com/visionectmobileble/MainActivity.java
@@ -2,7 +2,24 @@ package com.visionect.mobile.ble;
 
 import com.facebook.react.ReactActivity;
 
+import java.util.List;
+import java.util.Arrays;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends ReactActivity {
+
+    private static final int RC_SIGN_IN = 123456;
+
+    FirebaseAuth mAuth;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -11,5 +28,57 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "VisionectMobileBle";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main);
+
+        // Initialize the FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        // Check if there's a signed in user
+        if (mAuth.getCurrentUser() != null) {
+            // Users is signed in
+            AuthUI.getInstance()
+                .signOut(this);
+        } else {
+            // There's no signed in user
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN
+            );
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, "Login Success!", Toast.LENGTH_SHORT).show();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Toast.makeText(this, "Login Error!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
